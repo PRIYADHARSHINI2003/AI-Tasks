1ad60c2c7683e97930df0df59541b13a57d3ee30
PRIYADHARSHINI2003 <73331349+PRIYADHARSHINI2003@users.noreply.github.com>
2021-05-29 20:51:02 +0530

    Update Task1.py

--- Task-1/Task1.py
+++ Task-1/Task1.py
@@ -1,1 +1,30 @@
+file = input("Enter the filename:") 
+h = open(file, 'r')
+ 
+content = h.readlines()
 
+  
+list = []
+for i in range(len(content)):
+    word = ""
+    j=0
+    while content[i][j] !='\n':
+        word += content[i][j]
+        j+=1
+        if j == len(content[i]):
+            break
+    list.append(float(word)) 
+Minimum = list[0]
+Maximum = list[0]
+sum= 0
+for i in range(len(list)):
+    if(list[i]>Maximum):
+        Maximum = list[i]
+    elif(list[i]<Minimum):
+        Minimum = list[i]
+
+    sum += list[i]
+    length = len(list)
+print("Minimum: " + str(Minimum))
+print("Maximum: " + str(Maximum))
+print("Average: " + str(sum/length))
